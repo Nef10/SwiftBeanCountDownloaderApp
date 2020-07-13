c20cc6e335002dd1d908d7cfd722a3fb09db8997
Steffen Kötte <steffen.koette@gmail.com>
2020-07-12 22:03:22 -0700

    Initial Commit

---
+++ SwiftBeanCountDownloaderApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftBeanCountDownloaderApp
+//
+//  Created by Steffen Kötte on 2020-07-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftBeanCountDownloaderApp/SwiftBeanCountDownloaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBeanCountDownloaderAppApp.swift
+//  SwiftBeanCountDownloaderApp
+//
+//  Created by Steffen Kötte on 2020-07-12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBeanCountDownloaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
