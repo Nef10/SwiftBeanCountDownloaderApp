d5ab098df9cd5e9393d34743c21b59e67a7990d3
Steffen Kötte <steffen.koette@gmail.com>
2020-08-01 00:12:03 -0700

    Add test targets

---
+++ SwiftBeanCountDownloaderAppUITests/SwiftBeanCountDownloaderAppUITests.swift
@@ -0,0 +1,12 @@
+//
+//  SwiftBeanCountDownloaderAppUITests.swift
+//  SwiftBeanCountDownloaderAppUITests
+//
+//  Created by Steffen Kötte on 2020-08-01.
+//
+
+import XCTest
+
+class SwiftBeanCountDownloaderAppUITests: XCTestCase {
+
+}
